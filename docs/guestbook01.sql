9f569721ff958432496faef2d3a78dab6c7e7dd6
hakyeonghoon1 <skyhoon8@gmail.com>
2021-10-08 17:35:04 +0900

    guestbook02 commit

--- docs/guestbook01.sql
+++ docs/guestbook01.sql
@@ -10,6 +10,3 @@ select no, name, date_format(reg_date,'%Y/%m/%d %H:%i:%s'), message from guestbo
 delete from guestbook where no = 3 and password = '1234';
 
 
-ALTER TABLE guestbook AUTO_INCREMENT=1;
-SET @COUNT = 0;
-UPDATE guestbook SET no = @COUNT:=@COUNT+1;
